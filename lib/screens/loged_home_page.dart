5671e693e934616ae6078746d81d7022b78cae8e
MarvinFlm <marvin.flamand@epitech.eu>
2022-12-14 11:41:20 +0100

    fix : include

--- lib/screens/loged_home_page.dart
+++ lib/screens/loged_home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:prototype_1/styles/colors.dart';
