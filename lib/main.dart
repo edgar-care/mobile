35190a6ede9431ef99e6be1692d9cc7369ebf9d5
Marvin <marvin.flamand@epitech.eu>
2023-05-19 19:11:19 +0200

    feat: restart all

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:prototype_1/screens/info_page.dart';
-import 'screens/loged_documents_page.dart';
-import 'screens/loged_home_page.dart';
-import 'screens/chat_box_page.dart';
-import 'screens/login_hint_page.dart';
-import 'screens/no_loged_start_analayse_page.dart';
-import 'screens/doctors_list_page.dart';
-
+import 'screens/landing_page.dart';
 Future main() async {
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
@@ -19,21 +12,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'edgar',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.blue,
           useMaterial3: true,
           fontFamily: 'Raleway'),
       routes: {
-        '/': (context) =>
-            const StartTheAnalyse(title: 'StartTheAnalyseNoLoged'),
-        '/home': (context) => const LogedHomePage(title: 'home'),
-        '/chat': (context) => const ChatBoxPage(title: 'chat'),
-        '/login': (context) => const LoginHintPage(title: 'login'),
-        '/info': (context) => const InfoPage(title: 'info'),
-        '/doctors': (context) => const DoctorsListPage(title: 'doctors'),
-        '/documents': (context) => const LogedDocumentPage(title: 'documents'),
+        '/': (context) => const LandingPage(),
+        '/info': ,
+        '/login': ,
+        '/auth': ,
+        '/register': ,
+        '/register_validate': ,
       },
     );
   }
