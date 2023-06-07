979089e540875bd4e6bdf184a13a17e0f9bf816f
Marvin <marvin.flamand@epitech.eu>
2023-06-07 22:39:22 +0200

    feat(component): add component button

---
+++ lib/widget/plain_button.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:prototype_1/styles/colors.dart';
+
+class PlainButton extends StatelessWidget {
+  final String text;
+  final Function() onPressed;
+  const PlainButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        backgroundColor: AppColors.buttonBlue,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(text,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+              color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold)),
+    );
+  }
+}
+
+class PlainBorderButton extends StatelessWidget {
+  final String text;
+  final Function() onPressed;
+  const PlainBorderButton(
+      {super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        backgroundColor: AppColors.buttonBlue,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+          side: const BorderSide(width: 2, color: AppColors.blue400),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(text,
+          style: const TextStyle(
+              color: Colors.white, fontSize: 12, fontWeight: FontWeight.bold)),
+    );
+  }
+}
