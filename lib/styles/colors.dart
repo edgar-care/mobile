6b0dfe9d1d423c50f75ae118aeb7b0f55c36b7cb
Marvin <marvin.flamand@epitech.eu>
2023-06-07 22:34:27 +0200

    feat : add landing page and component navbar

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -5,4 +5,15 @@ class AppColors {
   static const buttonBlue = Color.fromRGBO(51, 95, 194, 1);
   static const textBlue = Color.fromRGBO(46, 76, 154, 1);
   static const lightBlue = Color.fromRGBO(198, 222, 247, 1);
+  static const blue950 = Color.fromRGBO(30, 43, 77, 1);
+  static const blue900 = Color.fromRGBO(43, 68, 125, 1);
+  static const blue800 = Color.fromRGBO(46, 76, 154, 1);
+  static const blue700 = Color.fromRGBO(51, 95, 194, 1);
+  static const blue600 = Color.fromRGBO(60, 115, 218, 1);
+  static const blue500 = Color.fromRGBO(81, 141, 224, 1);
+  static const blue400 = Color.fromRGBO(114, 172, 232, 1);
+  static const blue300 = Color.fromRGBO(160, 201, 240, 1);
+  static const blue200 = Color.fromRGBO(198, 222, 247, 1);
+  static const blue100 = Color.fromRGBO(223, 236, 250, 1);
+  static const blue50 = Color.fromRGBO(241, 247, 253, 1);
 }
