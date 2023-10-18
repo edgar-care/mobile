848d94dfc9015dcf7ceae485df239718a0bd0858
Marvin <marvin.flamand@epitech.eu>
2023-10-18 12:24:12 +0200

    feat: add all widget agenda

--- lib/screens/dashboard/gestion_rendez_vous.dart
+++ lib/screens/dashboard/gestion_rendez_vous.dart
@@ -10,6 +10,30 @@ class GestionRendezVous extends StatefulWidget {
 }
 
 class _GestionRendezVousPageState extends State<GestionRendezVous> {
+
+    final List<Map<String, String>> rdv = [
+    {
+      'date': '10/18/2023',
+      'heure': '23:59:59',
+      'medecin': 'Dr. Dupont',
+      'adresse': '123 Rue de la Santé, Paris'
+    },
+    {
+      'date': '10/20/2023',
+      'heure': '23:59:59',
+      'medecin': 'Dr. Dupont',
+      'adresse': '123 Rue de la Santé, Paris'
+    },
+    {
+      'date': '17/09/2023',
+      'heure': '23:59:59',
+      'medecin': 'Dr. Dupont',
+      'adresse': '123 Rue de la Santé, Paris'
+    },
+  ];
+
+  final currentDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -23,36 +47,28 @@ class _GestionRendezVousPageState extends State<GestionRendezVous> {
           ),
         ),
         const SizedBox(height: 30),
-        DateSlider(),
+        DateSlider(rdv: rdv),
+        const SizedBox(height: 30),
+        const SwitchThreeElements(),
+        const SizedBox(height: 10),
+        Expanded(
+          child: ListView.builder(
+            itemCount: rdv.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              final filteredRdv = rdv.where((element) => element['date'] != null && element['date']!.isNotEmpty && DateTime.parse(element['date']!).isAfter(DateTime.now())).toList();
+              return CardRdv(rdv: filteredRdv[index % filteredRdv.length]);
+            },
+          ),
+        ),
       ],
     );
   }
 }
 
 class DateSlider extends StatelessWidget {
-  // ignore: use_key_in_widget_constructors
-  DateSlider({Key? key});
-
-  final List<Map<String, String>> rdv = [
-    {
-      'date': '10/9/2023',
-      'heure': '23:59:59',
-      'medecin': 'Dr. Dupont',
-      'adresse': '123 Rue de la Santé, Paris'
-    },
-    {
-      'date': '10/20/2023',
-      'heure': '23:59:59',
-      'medecin': 'Dr. Dupont',
-      'adresse': '123 Rue de la Santé, Paris'
-    },
-    {
-      'date': '17/9/2023',
-      'heure': '23:59:59',
-      'medecin': 'Dr. Dupont',
-      'adresse': '123 Rue de la Santé, Paris'
-    },
-  ];
+  final List<Map<String, String>> rdv;
+  const DateSlider({super.key, required this.rdv});
 
   @override
   Widget build(BuildContext context) {
@@ -141,6 +157,202 @@ class DateCard extends StatelessWidget {
               fontSize: 16,
               fontWeight: FontWeight.bold,
               color: isToday ? Colors.white : AppColors.grey500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SwitchThreeElements extends StatefulWidget {
+  const SwitchThreeElements({super.key});
+
+ @override
+ // ignore: library_private_types_in_public_api
+ _SwitchThreeElementsState createState() => _SwitchThreeElementsState();
+}
+
+class _SwitchThreeElementsState extends State<SwitchThreeElements> {
+ int _selectedIndex = 0;
+
+ @override
+ Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: 
+      Container(
+        height: 60.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(50.0),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 2),
+              blurRadius: 4.0,
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            for (int i = 0; i < 3; i++)
+              InkWell(
+                onTap: () {
+                setState(() {
+                    _selectedIndex = i;
+                });
+                },
+                child: Container(
+                width: MediaQuery.of(context).size.width / 3 - 12.0,
+                  height: 100.0,
+                  alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: _selectedIndex == i ? AppColors.blue700 : Colors.white,
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                  child: Text(
+                    ['A venir', 'Passés', 'Annuler'][i],
+                    style: TextStyle(fontSize: 16.0, color: _selectedIndex == i ? Colors.white : AppColors.blue950),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardRdv extends StatefulWidget {
+  final Map<String, String> rdv;
+
+  const CardRdv({super.key, required this.rdv});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _CardRdvState createState() => _CardRdvState();
+}
+
+class _CardRdvState extends State<CardRdv> {
+  @override
+  Widget build(BuildContext context) {
+    var currenDate = DateTime.now();
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                widget.rdv['date'] == DateFormat.yMd().format(currenDate) ? 'Aujourd\'hui' : widget.rdv['date'] ?? '',
+                style: const TextStyle(
+                  color: AppColors.green500,
+                  fontFamily: 'Poppins',
+                  fontSize: 16.0,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w600,
+                  height: null,
+                ),
+              ),
+              Text(
+                widget.rdv['heure']?.replaceAll(':', 'h').substring(0, 5) ?? '',
+                style: const TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 16.0,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(width: 16),
+          Container(
+            width: MediaQuery.of(context).size.width - 140,
+            decoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(color: Colors.grey[300]!),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          widget.rdv['medecin'] ?? '',
+                          style: const TextStyle(
+                            color: AppColors.grey950,
+                            fontFamily: 'Poppins',
+                            fontSize: 16.0,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w600,
+                            height: null,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.more_vert, color: AppColors.grey500, size: 20),
+                          onPressed: () {
+                            
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: SizedBox(
+                      height: 1,
+                      width: double.infinity - 32,
+                      child: Container(
+                        color: Colors.grey[300],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Icon(Icons.location_on, color: AppColors.grey500, size: 20),
+                        Text(
+                          widget.rdv['adresse'] ?? '',
+                          style: const TextStyle(
+                            color: AppColors.grey500,
+                            fontFamily: 'Poppins',
+                            fontSize: 12.0,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w500,
+                            height: null,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.arrow_forward_ios
+                          , color: AppColors.grey500, size: 20),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ],
