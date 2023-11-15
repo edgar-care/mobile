200bc6baebfd937c0bd829a1c3d4aa1390782b49
SpiritGY <nicolas.jacquemin@epitech.eu>
2023-11-15 12:20:01 +0100

    [ADD] adding test for login page

---
+++ medecin_mobile/test/login_page_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:medecin_mobile/screens/auth/login_page.dart';
+import 'package:medecin_mobile/widgets/field_custom.dart';
+
+void main() {
+  testWidgets('Test de la page de login', (WidgetTester tester) async {
+    // Créez l'instance de la page de connexion
+    // ignore: avoid_types_as_parameter_names
+    final loginPage = MaterialApp(home: Login(callback: (int) {1;}));
+
+    // Ajoutez la page de connexion à l'arborescence des widgets
+    await tester.pumpWidget(loginPage);
+
+    // Vérifiez si les éléments sont présents sur la page
+    expect(find.text('Adresse mail'), findsOneWidget);
+    expect(find.text('Mot de passe'), findsOneWidget);
+    expect(find.text('Connexion'), findsOneWidget);
+    expect(find.text('Vous n\'êtes pas encore inscrit ?'), findsOneWidget);
+    expect(find.text('Créer un compte'), findsOneWidget);
+
+    // Vérifiez si les champs de texte sont vides
+    expect(find.widgetWithText(CustomField, ''), findsNWidgets(2));
+
+    // Remplissez les champs de texte
+    await tester.enterText(
+        find.widgetWithText(CustomField, '').first, 'test@example.com');
+    await tester.enterText(
+        find.widgetWithText(CustomField, '').last, 'password123');
+
+    // Vérifiez si la navigation vers la page d'aide a été effectuée
+  });
+}
