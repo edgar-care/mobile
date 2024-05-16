902d2361dd415cacf95104396bb07fa51da3043b
SpiritGY <nicolas.jacquemin@epitech.eu>
2024-05-16 15:49:29 +0200

    fix(all): fix for analyze

--- medecin_mobile/lib/services/login_service.dart
+++ medecin_mobile/lib/services/login_service.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -8,7 +10,6 @@ import 'package:edgar_pro/widgets/login_snackbar.dart';
 Future login(String email, String password, BuildContext context) async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   String url = '${dotenv.env['URL']}auth/d/login';
-  // ignore: use_build_context_synchronously
   ScaffoldMessenger.of(context).showSnackBar(InfoLoginSnackBar(
       message: "Connexion à l'application ...", context: context));
   final response = await http.post(
@@ -17,21 +18,16 @@ Future login(String email, String password, BuildContext context) async {
     body: jsonEncode({'email': email, 'password': password}),
   );
   if (response.statusCode == 200) {
-    // ignore: use_build_context_synchronously
     ScaffoldMessenger.of(context).removeCurrentSnackBar();
     prefs.setString('token', jsonDecode(response.body)['token']);
-    // ignore: use_build_context_synchronously
     ScaffoldMessenger.of(context).showSnackBar(SuccessLoginSnackBar(
       message: 'Connecté à l\'application',
       context: context,
     ));
     await Future.delayed(const Duration(seconds: 2));
-    // ignore: use_build_context_synchronously
     Navigator.pushNamed(context, '/dashboard');
   } else {
-    // ignore: use_build_context_synchronously
     ScaffoldMessenger.of(context).removeCurrentSnackBar();
-    // ignore: use_build_context_synchronously
     ScaffoldMessenger.of(context).showSnackBar(ErrorLoginSnackBar(
       message: 'Les identifiants ne correspondent pas !',
       context: context,
