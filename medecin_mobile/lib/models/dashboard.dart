94b4f6f010feaf4b13de225c374bb47efc013ee9
SpiritGY <nicolas.jacquemin@epitech.eu>
2024-06-14 18:48:18 +0200

    feat(navigation): adding patient navbar and multipages

--- medecin_mobile/lib/models/dashboard.dart
+++ medecin_mobile/lib/models/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:edgar_pro/screens/dashboard/agenda_page.dart';
+import 'package:animations/animations.dart';
 import 'package:edgar_pro/screens/dashboard/chat_page.dart';
 import 'package:edgar_pro/screens/dashboard/chat_patient_page.dart';
 import 'package:edgar_pro/screens/dashboard/document_page.dart';
@@ -21,7 +22,9 @@ class DashBoard extends StatefulWidget {
 
 class _DashBoardState extends State<DashBoard> {
   int _selectedIndex = 0;
+  int _previousIndex = 0;
   String _id = "";
+  final List<int> _navigationStack = [0];
 
   void updateSelectedIndex(int index) {
     setState(() {
@@ -46,6 +49,31 @@ class _DashBoardState extends State<DashBoard> {
   @override
   void initState() {
     super.initState();
+  }
+
+  void _onItemTapped(int index) {
+    if (index != _selectedIndex) {
+      setState(() {
+        _previousIndex = _selectedIndex;
+        _selectedIndex = index;
+        if (_navigationStack.isEmpty || _navigationStack.last != index) {
+          _navigationStack.add(index);
+        }
+      });
+    }
+  }
+
+  Future<bool> _onWillPop() async {
+    if (_navigationStack.length > 1) {
+      setState(() {
+        _selectedIndex = _previousIndex;
+        if (_navigationStack.isNotEmpty) {
+          _previousIndex = _navigationStack.last;
+        }
+      });
+      return false;
+    }
+    return true;
   }
 
   @override
@@ -74,26 +102,40 @@ class _DashBoardState extends State<DashBoard> {
       ),
       ChatPatient(id: getId(), setPages: updateSelectedIndex, setId: updateId)
     ];
-    return Scaffold(
-      backgroundColor: AppColors.blue50,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-          child: Stack(children: [
-            AnimatedSwitcher(
-              duration: const Duration(milliseconds: 600),
+
+
+    // ignore: deprecated_member_use
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            SafeArea(
               child: Padding(
-                padding: const EdgeInsets.only(
-                  top: 80,
-                ),
+                padding: const EdgeInsets.fromLTRB(16, 12, 16, 100),
+                child: PageTransitionSwitcher(
+                  duration: const Duration(milliseconds: 400),
+                  reverse: _previousIndex > _selectedIndex,
+                  transitionBuilder: (
+                    Widget child,
+                    Animation<double> primaryAnimation,
+                    Animation<double> secondaryAnimation,
+                  ) {
+                    return FadeThroughTransition(
+                      animation: primaryAnimation,
+                      secondaryAnimation: secondaryAnimation,
+                      child: child,
+                    );
+                  },
                 child: pages[_selectedIndex],
               ),
             ),
-            CustomAppBar(
-              callback: updateSelectedIndex,
-              getSelected: getSelectedIndex,
             ),
-          ]),
+            CustomBottomBar(
+              selectedIndex: _selectedIndex,
+              onItemTapped: _onItemTapped,
+       ),
+          ],
         ),
       ),
     );
