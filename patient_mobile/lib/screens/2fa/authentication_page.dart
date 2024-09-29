b64d963a32b24f6529af3a1a3d29c00718b28b43
SpiritGY <nicolas.jacquemin@epitech.eu>
2024-09-29 21:56:09 +0200

    fix(2fa) : refresh secret state on 2fa page for patient app

--- patient_mobile/lib/screens/2fa/authentication_page.dart
+++ patient_mobile/lib/screens/2fa/authentication_page.dart
@@ -54,6 +54,7 @@ class _DoubleAuthenticationState extends State<DoubleAuthentication> {
       emailActive = false;
       thirdActive = false;
       mobileActive = false;
+      secret = false;
     });
     getEnable2fa().then((value) {
       Logger().d(value);
