634f607b66734bc5c1ecf72e0832e883a716fc4d
Marvin Flm <72012603+MarvinFlm@users.noreply.github.com>
2024-11-15 17:07:56 +0100

    fix: analyze

--- patient_mobile/lib/models/onboarding.dart
+++ patient_mobile/lib/models/onboarding.dart
@@ -897,30 +897,6 @@ class _Onboarding3State extends State<Onboarding3> {
                       ),
                     );
                   }
-                  List<String> parts = birthdate.split('/');
-                  String americanDate = '${parts[2]}-${parts[1]}-${parts[0]}';
-                  final birth = DateTime.parse(americanDate);
-                  final integerDate =
-                      (birth.millisecondsSinceEpoch / 1000).round();
-
-                  final Map<String, Object> body = {
-                    "name": lastname,
-                    "firstName": name,
-                    "birthdate": integerDate,
-                    "sex": sexe,
-                    "weight": int.parse(weight) * 100,
-                    "height": int.parse(height),
-                    "primary_doctor_id": primaryDoctorId,
-                    "family_members_med_info_id": [],
-                    "medical_antecedents": traitments,
-                  };
-
-                  var reponse = await postMedicalInfo(body);
-                  if (reponse == true) {
-                    widget.updateSelectedIndex(3);
-                  } else {}
-
-                  widget.updateSelectedIndex(3);
                 },
               ),
             ),
@@ -963,12 +939,13 @@ class _Onboarding3State extends State<Onboarding3> {
             ),
             const SizedBox(height: 8),
             Buttons(
-                variant: Variant.secondary,
-                size: SizeButton.md,
-                msg: const Text("Précédent"),
-                onPressed: () {
-                  widget.updateSelectedIndex(1);
-                }),
+              variant: Variant.secondary,
+              size: SizeButton.md,
+              msg: const Text("Précédent"),
+              onPressed: () {
+                widget.updateSelectedIndex(1);
+              },
+            ),
           ],
         ),
       ),
