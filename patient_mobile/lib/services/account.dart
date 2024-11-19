3de1d346235cbb4b3fc92ead78c99681301af34f
Marvin Flm <72012603+MarvinFlm@users.noreply.github.com>
2024-11-19 15:28:29 +0100

    fix: update patient

--- patient_mobile/lib/services/account.dart
+++ patient_mobile/lib/services/account.dart
@@ -1,6 +1,6 @@
-import 'dart:convert';
 import 'package:edgar_app/services/request.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 Future disableAccount(BuildContext context) async {
   await httpRequest(
@@ -29,15 +29,18 @@ Future deleteAccount(BuildContext context) async {
   );
 }
 
-Future resetPassword(String password, BuildContext context) async {
+Future<bool> updatePassword(
+    String oldPassword, String password, BuildContext context) async {
   final response = await httpRequest(
-      type: RequestType.post,
-      endpoint: 'auth/reset_password',
-      needsToken: true,
-      body: {"new_password": password},
-      context: context);
-  if (response.body != null) {
-    return jsonDecode(response.body)['devices'];
+    type: RequestType.post,
+    endpoint: 'auth/update_password',
+    needsToken: true,
+    body: {"old_password": oldPassword, "new_password": password},
+    context: context,
+  );
+  if (response != null) {
+    Logger().i(response);
+    return true;
   }
-  return response;
+  return false;
 }
